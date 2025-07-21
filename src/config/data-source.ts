7113cf3775d762c7c405b044fac36c1d875d27be
ricardo-henrique <ricardohenrique2013.1@gmail.com>
2025-07-21 18:44:21 -0300

    add environment variable for production

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -7,7 +7,7 @@ import { Category } from "../entity/Category";
 export const AppDataSource = new DataSource({
   type: "sqlite",
   database: "./src/db/cms.sqlite",
-  synchronize: true,
+  synchronize: process.env.NODE_ENV !== "production",
   logging: false,
   entities: [User, Post, Category],
   migrations: [],
