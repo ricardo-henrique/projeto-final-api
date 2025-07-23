1e33794399752e72464fafc00bd668b2b7c5e462
ricardo-henrique <ricardohenrique2013.1@gmail.com>
2025-07-22 21:14:50 -0300

    add cors options

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const allowedOrigins = ["http://localhost:4200", "https://cms-hardware-client.netlify.app"];
+
+const corsOptions: cors.CorsOptions = {
+  origin: function (origin, callback) {
+    // Permite requisições sem origem (como Postman ou requisições de arquivos locais)
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"],
+  credentials: true,
+};
+
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/v1/", apiRateLimiter);
